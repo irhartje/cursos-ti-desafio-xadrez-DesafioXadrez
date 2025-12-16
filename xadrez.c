87dd863e4161952509e41665ee0ea10ec0ebe878
irHartje <irhartje@gmail.com>
2025-12-16 20:56:53 +0000

    Adicionado movimentação do cavalo.

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int torre = 0, bispo = 0, rainha = 0;
+    int torre = 0, bispo = 0, rainha = 0, cavalo = 1;
 
     for (torre = 0; torre < 5; torre++)
         printf("Torre moveu %d casa(s) para direita.\n", torre);
@@ -16,7 +16,22 @@ int main()
     {
         rainha++;
     } while (rainha < 8);
-    printf("Rainha anda %d casa(s) para esquerda.", rainha);
+    printf("Rainha anda %d casa(s) para esquerda.\n", rainha);
 
+        while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cavalo anda uma casa para baixo. \n"); // Desce duas casas.
+        }
+        printf("Cavalo anda uma casa para esquerda\n"); // Uma casa para esquerda
+    }
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
